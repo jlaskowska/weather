4e7d8050fc6e9775152c5a28960df457affb3f11
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-01-28 16:54:45 +0100

    Added AppColors.

---
+++ lib/configs/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkBlue = const Color(0xff1820f1);
+  static const Color darkViolet = const Color(0xff4944d6);
+  static const Color shadowColor = const Color(0xff151cde);
+  static const Color lightViolet = const Color(0xffdddcfc);
+  static const Color lightRose = const Color(0xfff5f3fe);
+}
