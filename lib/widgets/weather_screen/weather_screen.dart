3198ea0a961a81ea302dabfd82a5e135cfbf7273
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-01-30 18:11:55 +0100

    Updated WeatherScreen.

--- lib/widgets/weather_screen/weather_screen.dart
+++ lib/widgets/weather_screen/weather_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weather/services/weather_service.dart';
+import 'package:provider/provider.dart';
+import 'package:weather/states/app_state.dart';
 import 'package:weather/widgets/weather_screen/weather_screen_content.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -10,26 +11,26 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  Future weatherFuture = WeatherService().getWeatherReportForCity('Berlin');
-
   @override
   Widget build(BuildContext context) {
+    final appState = Provider.of<AppState>(context);
+
     return FutureBuilder(
-      future: weatherFuture,
+      future: appState.getWeather(),
       builder: (_, snapshot) {
         if (snapshot.hasData) {
           return WeatherScreenContent(snapshot.data);
         } else if (snapshot.hasError || (snapshot.connectionState == ConnectionState.done && snapshot.data == null)) {
           return AlertDialog(
             title: Text('Ooops'),
-            content: Text('Something went wrong. Make sure you have an internet connection.'),
+            content: Text(
+                'Something went wrong. Make sure you enabled location services and have an active internet connection.'),
             actions: <Widget>[
               FlatButton(
                 child: Text('Try Again'),
                 onPressed: () {
-                  setState(() {
-                    weatherFuture = WeatherService().getWeatherReportForCity('Berlin');
-                  });
+                  // TODO better way to trigger rebuild?
+                  setState(() {});
                 },
               )
             ],
