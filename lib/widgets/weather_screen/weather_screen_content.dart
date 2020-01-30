033134f78b0bfee01585bdfe7d788df93fca043f
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-01-30 18:11:23 +0100

    Added scrollability to WeatherScreenContent.

--- lib/widgets/weather_screen/weather_screen_content.dart
+++ lib/widgets/weather_screen/weather_screen_content.dart
@@ -14,45 +14,62 @@ class WeatherScreenContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Text(
-            weatherReport.title.toUpperCase(),
-            style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 50,
-              color: AppColors.lightRose,
-            ),
+    return LayoutBuilder(builder: (BuildContext context, BoxConstraints viewportConstraints) {
+      return SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: viewportConstraints.maxHeight,
           ),
-          Text(
-            forecastToday.weatherDescription,
-            style: TextStyle(color: AppColors.lightViolet, fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          WeatherForecastWidget(
-            forecastToday,
-            iconSize: 120.0,
-            sizedBoxHeight: 50.0,
-            temperatureFontSize: 60.0,
-            dateFontSize: 20,
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Expanded(
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                for (WeatherForecast forecast in forecastOtherDays)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0, right: 20),
-                    child: WeatherForecastWidget(forecast),
-                  )
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 32,
+                ),
+                Text(
+                  weatherReport.title.toUpperCase(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 50,
+                    color: AppColors.lightRose,
+                  ),
+                ),
+                Text(
+                  forecastToday.weatherDescription,
+                  style: TextStyle(
+                    color: AppColors.lightViolet,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                WeatherForecastWidget(
+                  forecastToday,
+                  iconSize: 120.0,
+                  sizedBoxHeight: 50.0,
+                  temperatureFontSize: 60.0,
+                  dateFontSize: 20,
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Container(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      for (WeatherForecast forecast in forecastOtherDays)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20.0, right: 20),
+                          child: WeatherForecastWidget(forecast),
+                        )
+                    ],
+                  ),
+                ),
               ],
             ),
-          )
-        ],
-      ),
-    );
+          ),
+        ),
+      );
+    });
   }
 }
