6219b4440aa41f8ad33a47f60d56ddb621f60171
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-01-30 16:53:09 +0100

    Updated AppState.
    Persist isCelsius to disk.

--- lib/states/app_state.dart
+++ lib/states/app_state.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppState with ChangeNotifier {
-  bool _isCelsius = true;
+  static SharedPreferences _sharedPreferences;
+
+  AppState() {
+    _init();
+  }
+
+  void _init() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  // isCelsius
+
+  static const _isCelsiusKey = 'isCelsius';
+
+  bool _isCelsius = _sharedPreferences?.getBool(_isCelsiusKey) ?? true;
 
   bool get isCelsius => _isCelsius;
 
   set isCelsius(bool value) {
     _isCelsius = value;
+    _sharedPreferences.setBool(_isCelsiusKey, value);
     notifyListeners();
   }
 }
